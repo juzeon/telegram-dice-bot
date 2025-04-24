6af14ffe19c9dc7caad9a155a9c6d2db64fdaf07
juzheng <skyjuzheng@gmail.com>
2025-04-24 16:46:37 +0800

    add dice

--- src/bot.rs
+++ src/bot.rs
@@ -1,28 +1,78 @@
 use crate::types::Config;
+use anyhow::{Context, bail};
+use rand::Rng;
+use regex::Regex;
+use std::error::Error;
+use std::ops::{Range, RangeInclusive};
+use std::str::FromStr;
+use std::sync::LazyLock;
 use teloxide::Bot;
 use teloxide::dispatching::{HandlerExt, UpdateFilterExt};
+use teloxide::payloads::SendMessageSetters;
 use teloxide::prelude::{Dispatcher, Message, Requester, Update};
 use teloxide::requests::ResponseResult;
 use teloxide::sugar::request::RequestReplyExt;
+use teloxide::types::{ParseMode, ReplyParameters};
 use teloxide::utils::command::BotCommands;
+use tracing::info;
 
+static DICE_RE: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r"^(\d*)[dD](\d+)((?:[+-]\d+)*)(?: +(.*))?$").unwrap());
+static DICE_FIXES_RE: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"([+-]\d+)").unwrap());
 #[derive(teloxide::macros::BotCommands, Clone)]
 #[command(
     rename_rule = "lowercase",
-    description = "These commands are supported:"
+    description = "骰子语法：
+- <code>d10 投1个10面骰，空格之后可以写注释（也可以不写）</code>
+- <code>2d100 投两个100面骰</code>
+- <code>d10+3 投1个10面骰，修正值+3</code>
+- <code>2d10+3 投2个10面骰，每个修正值+3</code>
+- <code>2d10-3 也可以-3</code>
+- <code>2d10+3+5 对每个骰子赋予不同修正值</code>
+- <code>3d10+30-50+40</code>
+
+命令：
+"
 )]
 enum DiceCommand {
-    #[command(description = "display help")]
+    #[command(description = "显示帮助")]
     Start,
-    #[command(description = "display this text")]
+    #[command(description = "显示帮助")]
     Help,
-    #[command(description = "roll a YesOrNo")]
+    #[command(description = "选一个YesOrNo")]
     YesOrNo,
 }
 #[derive(Clone, Debug)]
 pub struct DiceBot {
     config: Config,
     bot: Bot,
+}
+enum Fix {
+    Add(usize),
+    Sub(usize),
+}
+impl Fix {
+    fn get_value(&self) -> usize {
+        match self {
+            Fix::Add(num) => *num,
+            Fix::Sub(num) => *num,
+        }
+    }
+}
+impl FromStr for Fix {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let sign = &s[0..1];
+        let num = s[1..].parse::<usize>()?;
+        match sign {
+            "+" => Ok(Self::Add(num)),
+            "-" => Ok(Self::Sub(num)),
+            _ => {
+                bail!("not a sign")
+            }
+        }
+    }
 }
 impl DiceBot {
     pub async fn new() -> DiceBot {
@@ -47,10 +97,11 @@ impl DiceBot {
             .dispatch()
             .await;
     }
-    async fn command_handler(bot: Bot, msg: Message, cmd: DiceCommand) -> ResponseResult<()> {
+    async fn command_handler(bot: Bot, msg: Message, cmd: DiceCommand) -> anyhow::Result<()> {
         match cmd {
             DiceCommand::Help | DiceCommand::Start => {
                 bot.send_message(msg.chat.id, DiceCommand::descriptions().to_string())
+                    .parse_mode(ParseMode::Html)
                     .reply_to(msg.id)
                     .await?;
             }
@@ -62,8 +113,84 @@ impl DiceBot {
         }
         Ok(())
     }
-    async fn text_handler(bot: Bot, msg: Message) -> ResponseResult<()> {
-        bot.send_message(msg.chat.id, "text").await?;
+    async fn reply(bot: Bot, msg: &Message, text: &str) -> anyhow::Result<()> {
+        bot.send_message(msg.chat.id, text)
+            .parse_mode(ParseMode::Html)
+            .reply_to(msg.id)
+            .await?;
         Ok(())
+    }
+    async fn text_handler(bot: Bot, msg: Message) -> anyhow::Result<()> {
+        let caps = DICE_RE.captures(msg.text().context("message empty")?);
+        if caps.is_none() {
+            Self::reply(bot, &msg, "骰子语法不正确，请使用 /help 查看帮助").await?;
+            return Ok(());
+        }
+        let caps = caps.unwrap();
+        let count = caps
+            .get(1)
+            .map(|x| x.as_str())
+            .unwrap_or("1")
+            .parse::<usize>()
+            .unwrap_or(1);
+        let dimension = caps
+            .get(2)
+            .map(|x| x.as_str())
+            .unwrap_or("10")
+            .parse::<usize>()
+            .unwrap_or(10);
+        let fixes = caps.get(3).map(|x| x.as_str()).unwrap_or("");
+        let comment = caps.get(4).map(|x| x.as_str()).unwrap_or("");
+        let mut fix_caps = None;
+        if fixes != "" {
+            fix_caps = Some(DICE_FIXES_RE.captures_iter(fixes).collect::<Vec<_>>());
+            let len = fix_caps.as_ref().unwrap().len();
+            if len != count && len != 1 {
+                Self::reply(bot, &msg, "修正值和骰子面数不匹配").await?;
+                return Ok(());
+            }
+        }
+        info!(?caps, count, dimension, fixes, comment, ?fix_caps, "Dice");
+        let mut res_arr: Vec<String> = vec![];
+        for i in 1..=count {
+            let roll = Self::get_random(1..=dimension);
+            let prefix = if count != 1 {
+                &format!("第{}个骰子：", i)
+            } else {
+                ""
+            };
+            if let Some(fix_caps) = fix_caps.as_ref() {
+                let fix: Fix = fix_caps[if fix_caps.len() == 1 { 0 } else { i - 1 }][1].parse()?;
+                let final_value: isize;
+                let sign;
+                match fix {
+                    Fix::Add(num) => {
+                        final_value = (roll + num) as isize;
+                        sign = "+";
+                    }
+                    Fix::Sub(num) => {
+                        final_value = (roll as isize) - (num as isize);
+                        sign = "-";
+                    }
+                };
+                res_arr.push(format!(
+                    "{}{roll} {sign} {} = {final_value}",
+                    prefix,
+                    fix.get_value()
+                ))
+            } else {
+                res_arr.push(format!("{}{}", prefix, roll))
+            }
+        }
+        let prefix = if comment == "" {
+            ""
+        } else {
+            &format!("<b>{comment}：</b>\n\n")
+        };
+        Self::reply(bot, &msg, &format!("{prefix}{}", res_arr.join("\n"))).await?;
+        Ok(())
+    }
+    fn get_random(range: RangeInclusive<usize>) -> usize {
+        rand::rng().random_range(range)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod types;
 
 use crate::bot::DiceBot;
 use teloxide::prelude::*;
-use tracing::{debug, instrument};
+use tracing::{debug, info, instrument};
 use tracing_subscriber::{Layer, filter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
@@ -12,9 +12,10 @@ async fn main() {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| format!("{}=debug", env!("CARGO_CRATE_NAME")).into()),
+                .unwrap_or_else(|_| format!("{}=debug,teloxide=info", env!("CARGO_CRATE_NAME")).into()),
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
+    info!("Starting bot");
     DiceBot::new().await.launch().await;
 }
